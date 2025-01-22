9ee32fefd087f1de39709bf5e46f7cf447d8a736
Rémi Bastide <Remi.Bastide@irit.fr>
2025-01-22 18:51:03 +0100

    Test commit sur le journal

--- src/test/java/bank/CommitBankingTest.java
+++ src/test/java/bank/CommitBankingTest.java
@@ -2,6 +2,7 @@ package bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import bank.dao.JournalRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,11 +17,10 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2 // @Log4j2 est une annotation qui permet d'ajouter un logger à la classe
 // On n'utilise pas @DataJpaTest, qui fait un "rollback" systématique après chaque test
-@SpringBootTest 
+@SpringBootTest
 class CommitBankingTest {
 	static final int ID_DU_DEBITEUR = 0;
 	static final int ID_DU_CREDITEUR = 1;
-	static final int ID_INCONNU = 99;
 	static int MONTANT_A_TRANSFERER = 10;
 
 	Account compteDebiteur;
@@ -28,36 +28,42 @@ class CommitBankingTest {
 
 	int balanceInitialeDebiteur;
 	int balanceInitialeCrediteur;
+    long journalEntriesBefore;
 
-	@Autowired 
+
+    @Autowired
 	BankService bankService;
+    @Autowired
+    JournalRepository journalDao;
 
 
-	@Autowired 
+	@Autowired
 	AccountRepository dao;
 
-	@BeforeEach() 
+	@BeforeEach()
 	void initialiserLesDonneesDeTest() {
 		log.info("Initialisations avant la transaction");
 		compteDebiteur = dao.findById(ID_DU_DEBITEUR).orElseThrow();
 		compteCrediteur   = dao.findById(ID_DU_CREDITEUR).orElseThrow();
 		balanceInitialeDebiteur = compteDebiteur.getBalance();
 		balanceInitialeCrediteur = compteCrediteur.getBalance();
-	}	
-	
+        journalEntriesBefore = journalDao.count();
+	}
+
 	@Test
 	void unTransfertBancaireReussit() {
 		log.info("Début d'un transfert autorisé");
 		// Given: les données définies dans 'data.sql'
 		// When: on exécute un transfert bancaire autorisé
 		bankService.transferMoney(ID_DU_DEBITEUR, ID_DU_CREDITEUR, MONTANT_A_TRANSFERER);
+        assertEquals(journalEntriesBefore + 1, journalDao.count(), "Journal entries should have increased by 1");
 		log.info("Fin d'un transfert autorisé");
 	}
 
-	
-	@AfterEach() 
+
+	@AfterEach()
 	void verifieTransfertOk() {
-		log.debug("Vérifications après la transaction");		
+		log.debug("Vérifications après la transaction");
 		// On rafraîchit les entités pour avoir les dernières mises à jour
 		// réalisées par la transaction de transfert
 		compteDebiteur   = dao.findById(ID_DU_DEBITEUR).orElseThrow();
@@ -66,6 +72,6 @@ class CommitBankingTest {
 		assertEquals(balanceInitialeDebiteur - MONTANT_A_TRANSFERER, compteDebiteur.getBalance(),
 				"Balance of debtor account should be decreased by the amount of the transfer");
 		assertEquals(balanceInitialeCrediteur + MONTANT_A_TRANSFERER, compteCrediteur.getBalance(),
-				"Balance of creditor account should be increased by the amount of the transfer");	
+				"Balance of creditor account should be increased by the amount of the transfer");
 	}
 }
